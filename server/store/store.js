92584f16c59e73c723d302bb6519e91c94639d05
resheet <resheetschultz@gmail.com>
2020-02-14 09:02:07 -0800

    associate client with lobby and vice versa

--- server/store/store.js
+++ server/store/store.js
@@ -13,11 +13,23 @@ class Store {
       socket,
       lobby: null
     }
-    console.log(id)
   }
 
   setClientLobby = (clientId, lobbyId) => {
-    this.state.clients[clientId].lobby = lobbyId;
+    let client = this.state.clients[clientId];
+    let newLobby = this.state.lobbies[lobbyId];
+    let oldLobby = this.state.lobbies[client.lobby];
+
+    //remove client from current lobby
+    if (oldLobby) {
+      oldLobby.members.splice(oldLobby.members.indexOf(clientId), 1);
+    }
+
+    //add client to new lobby
+    client.lobby = lobbyId;
+    if (!newLobby.members.includes(clientId)) {
+      newLobby.members.push(clientId);
+    }
   }
 
   addLobby = (id, name) => {
