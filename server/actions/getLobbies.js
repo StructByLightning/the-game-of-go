317a9c54f8de9abe6ad3da992c8db2bb948bb666
StructByLightning <mossygreenrock@gmail.com>
2020-02-13 18:43:06 -0800

    get lobby request

--- server/actions/getLobbies.js
+++ server/actions/getLobbies.js
@@ -3,5 +3,15 @@ import store from "../store/store.js";
 export default function (message, socket) {
   console.log("Get lobbies", store.state.lobbies);
 
+  socket.send(JSON.stringify({
+    error: null,
+    meta: {
+      clientId: message.meta.clientId
+    },
+    type: "REQUEST_GET_LOBBIES_FINISHED",
+    payload: {
+      lobbies: store.state.lobbies
+    }
+  }));
 
 };
--- server/store/store.js
+++ server/store/store.js
@@ -1,13 +1,36 @@
 class Store {
   constructor() {
     this.state = {
-      sockets: {}
+      sockets: {},
+      lobbies: {
+        "lobby-id-1": {
+          lobbyId: "lobby-id-1",
+          lobbyName: "lobby-1",
+          members: []
+        },
+        "lobby-id-2": {
+          lobbyId: "lobby-id-2",
+          lobbyName: "lobby-2",
+          members: []
+        }
+      },
     };
+
   }
 
   associateIdWithSocket = (id, socket) => {
     this.state.sockets[id] = socket;
   }
+
+  addLobby = (id, name) => {
+    this.state.lobbies[i] = {
+      lobbyId: id,
+      lobbyName: name,
+      members: []
+    }
+  }
+
+
 }
 
 const store = new Store();
