5eb3fd73d5c643b585394f4923378b4a4c3f0cbf
resheet <resheetschultz@gmail.com>
2020-02-14 09:01:41 -0800

    remove console logs

--- server/actions/joinLobby.js
+++ server/actions/joinLobby.js
@@ -1,12 +1,8 @@
 import store from "../store/store.js";
 
 export default function (message, socket) {
-  console.log("Join lobby");
 
   store.setClientLobby(message.meta.clientId, message.payload.lobbyId)
-
-  console.log(message.payload.lobbyId);
-  console.log(store.state.clients[message.meta.clientId])
 
   socket.send(JSON.stringify({
     error: null,
