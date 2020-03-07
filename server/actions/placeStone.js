1996ca59f9afd363d8f9a76e1b168b97520b6248
Tejas Menon <tejas.menon@hotmail.com>
2020-03-06 20:55:30 -0800

    Added placeStone algo

--- server/actions/placeStone.js
+++ server/actions/placeStone.js
@@ -30,37 +30,56 @@ export default function (message, socket) {
         }
   */
 
-  //I wrote this as a quick and dirty way to get some interactivity
-  //you will need to replace most of this with accessors
-  //also don't forget to verify legality
-  //and remove stones if they're surrounded
-
   let clientId = message.meta.clientId;
   let client = store.state.clients[clientId];
   let gameId = client.gameId;
-  let game = store.state.games[gameId];
+  if (gameId == null)
+    return;
 
+  let game = store.state.games[gameId];
   if (game == null)
     return;
 
-  let color = null;
-  if (game.black === clientId) {
-    color = "black";
-  } else if (game.white === clientId) {
-    color = "white";
+  let y = message.payload.y;
+  let x = message.payload.x;
+
+  let errorReply = {
+    error: "ERROR_ILLEGAL_MOVE",
+    meta: {
+      clientId
+    },
+    type: "REQUEST_PLACE_STONE_FINISHED",
+    payload: {
+      game
+    }
+  };
+
+  let clientTurnId = game.turn === "black" ? game.black : game.white;
+
+  if (game.board[y][x].state != null || clientId != clientTurnId) {
+    store.state.clients[clientId].socket.send(JSON.stringify(errorReply));
+    return;
+  }
+  let newBoard = JSON.parse(JSON.stringify(game.board)); //copy over board
+  newBoard[y][x].state = game.turn;
+  let oppCol = game.turn === "black" ? "white" : "black";
+
+  let toCapture = captureAll(newBoard, game.turn, oppCol);
+
+  if (toCapture.length == 0) {
+    let selfCapture = captureAll(newBoard, oppCol, game.turn);
+    if (selfCapture.length > 0) {
+      store.state.clients[clientId].socket.send(JSON.stringify(errorReply));
+      return;
+    }
   }
 
-  game.board[message.payload.y][message.payload.x].state = color;
+  game.board[y][x].state = game.turn;
+  game.turn = oppCol;
+  game.passes = 0;
+  toCapture.forEach(cell => game.board[cell.y][cell.x].state = null);
 
-  if (color === "black") {
-    game.turn = "white";
-  } else if (color === "white") {
-    game.turn = "black";
-  }
-
-  let members = game.members;
-
-  members.map((clientId) => {
+  game.members.map((clientId) => {
     store.state.clients[clientId].socket.send(JSON.stringify({
       error: null,
       meta: {
@@ -71,8 +90,47 @@ export default function (message, socket) {
         game
       }
     }));
-
   })
-
-
 };
+
+const contains = (array, cellA) => {
+  for (var cellB of array) {
+    if (cellA.x === cellB.x && cellA.y === cellB.y && cellA.state === cellB.state)
+      return true;
+  };
+  return false;
+}
+
+const captureAll = (board, guardCol, prisonCol) => {
+  let visited = [];
+  let allgroups = [];
+
+  board.forEach(row => {
+    row.forEach(cell => {
+      if (cell.state === prisonCol && !contains(visited, cell)) {
+        let group = [];
+        if (captureAllRec(board, cell, group, guardCol, prisonCol))
+          allgroups.push(...group);
+        visited.push(...group);
+      }
+    });
+  });
+  return allgroups;
+}
+
+const captureAllRec = (board, cell, group, guardCol, prisonCol) => {
+  group.push(cell);
+  let adjacencies = [[cell.x - 1, cell.y], [cell.x, cell.y - 1], [cell.x + 1, cell.y], [cell.x, cell.y + 1]];
+
+  for (var elem of adjacencies) {
+    if (elem[0] < 19 && elem[0] >= 0 && elem[1] < 19 && elem[1] >= 0) {
+      let adjCell = board[elem[1]][elem[0]];
+      if (adjCell.state === prisonCol && !contains(group, adjCell)) {
+        if (!captureAllRec(board, adjCell, group, guardCol, prisonCol))
+          return false;
+      } else if (adjCell.state != prisonCol && adjCell.state != guardCol)
+        return false;
+    }
+  };
+  return true;
+}
