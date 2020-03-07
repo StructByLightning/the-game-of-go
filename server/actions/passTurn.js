4aa7955207303ed4650a9c17dd5c391970e79d02
Tejas Menon <tejas.menon@hotmail.com>
2020-03-06 22:53:23 -0800

    Added pass feature

--- server/actions/passTurn.js
+++ server/actions/passTurn.js
@@ -44,7 +44,119 @@ export default function (message, socket) {
     }
   }
 
-  Once the game is over, this function will also remove the game from the games list, remove the lobby from lobbies,
-  and leave the clients as is until they disconnect. Post showing the scores, the front-end should redirect to main menu.
+  Once the game is over, the front-end shows the score and should the clients choose to disconnect, all their data 
+  is automatically deleted
   */
+
+  let clientId = message.meta.clientId;
+  let client = store.state.clients[clientId];
+  let game = store.state.games[client.gameId];
+
+  let clientTurnId = game.turn === "black" ? game.black : game.white;
+
+  let errorReply = {
+    error: "ERROR_ILLEGAL_PASS",
+    meta: {
+      clientId
+    },
+    type: "REQUEST_PASS_FINISHED",
+    payload: {
+      game
+    }
+  };
+
+  if (clientId != clientTurnId) {
+    store.state.clients[clientId].socket.send(JSON.stringify(errorReply));
+    return;
+  }
+
+  if (!game.passes) {
+    game.passes = 1;
+    game.turn = game.turn === "black" ? "white" : "black";
+    game.members.map((clientId) => {
+      store.state.clients[clientId].socket.send(JSON.stringify({
+        error: null,
+        meta: {
+          clientId
+        },
+        type: "REQUEST_PASS_FINISHED",
+        payload: {
+          game
+        }
+      }));
+    })
+    return;
+  }
+
+  if (game.passes == 1) {
+    game.passes = 2;
+    let black = captureAll(game.board, "black", null).length;
+    let white = captureAll(game.board, "white", null).length;
+
+    game.Board.forEach(row => {
+      row.forEach(cell => {
+        black += cell.state == "black" ? 1 : 0;
+        white += cell.state == "white" ? 1 : 0;
+      });
+    });
+
+    game.members.map((clientId) => {
+      store.state.clients[clientId].socket.send(JSON.stringify({
+        error: null,
+        meta: {
+          clientId
+        },
+        type: "REQUEST_GAME_OVER",
+        payload: {
+          game,
+          scores: {
+            white,
+            black
+          }
+        }
+      }));
+    });
+  }
 };
+
+const contains = (array, cellA) => {
+  for (var cellB of array) {
+    if (cellA.x === cellB.x && cellA.y === cellB.y && cellA.state === cellB.state)
+      return true;
+  };
+  return false;
+}
+
+const captureAll = (board, guardCol, prisonCol) => {
+  let visited = [];
+  let allgroups = [];
+
+  board.forEach(row => {
+    row.forEach(cell => {
+      if (cell.state === prisonCol && !contains(visited, cell)) {
+        let group = [];
+        if (captureAllRec(board, cell, group, guardCol, prisonCol))
+          allgroups.push(...group);
+        visited.push(...group);
+      }
+    });
+  });
+  return allgroups;
+}
+
+const captureAllRec = (board, cell, group, guardCol, prisonCol) => {
+  group.push(cell);
+  let adjacencies = [[cell.x - 1, cell.y], [cell.x, cell.y - 1], [cell.x + 1, cell.y], [cell.x, cell.y + 1]];
+
+  for (var elem of adjacencies) {
+    if (elem[0] < 19 && elem[0] >= 0 && elem[1] < 19 && elem[1] >= 0) {
+      let adjCell = board[elem[1]][elem[0]];
+      if (adjCell.state === prisonCol && !contains(group, adjCell)) {
+        if (!captureAllRec(board, adjCell, group, guardCol, prisonCol))
+          return false;
+      } else if (adjCell.state != prisonCol && adjCell.state != guardCol)
+        return false;
+    }
+  };
+  return true;
+}
