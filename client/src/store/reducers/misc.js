b25a92b64a1ef9edda382eb84e17bd63e1fb3c65
Raven <mossygreenrock@gmail.com>
2020-03-09 14:05:45 -0700

    .

--- client/src/store/reducers/misc.js
+++ client/src/store/reducers/misc.js
@@ -6,7 +6,7 @@ import Network from "clientNetwork/clientNetwork.js";
 
 export default class MiscReducer extends BaseReducer {
   initialState = {
-    serverUrl: "ws://ravenschultz.com:5000",
+    serverUrl: "wss://ravenschultz.com:5000",
     connected: false,
     clientId: "",
   };
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import fs from "fs";
 import network from "./network/network.js";
 const __dirname = path.resolve();
 
-/*uncomment for prod
+//uncomment for prod
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/ravenschultz.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/ravenschultz.com/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/ravenschultz.com/chain.pem', 'utf8');
@@ -30,7 +30,7 @@ httpsExpress.use(express.static("./build"));
 
 //otherwise just return the index since this is a spa
 httpsExpress.get("*", function response(req, res) {
-  res.sendFile(path.join(__dirname + "/../build/index.html"));
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 const httpsServer = https.createServer(credentials, httpsExpress);
@@ -55,8 +55,8 @@ const httpServer = http.createServer(httpExpress);
 httpServer.listen(80, () => {
   console.log('HTTP Server running on port 80');
 });
-*/
 
+/*
 
 //comment this http stuff out for prod
 
@@ -75,7 +75,8 @@ const httpServer = http.createServer(httpExpress);
 httpServer.listen(80, () => {
   console.log("Web server started (port 80)");
 });
+*/
 
 //socket server stuff
 
-network.listen(5000, http);
+network.listen(5000, https);
